dec94b52b40e34c8382a78e9a7ba90f335286100
mathru <user@mathru.net>
2022-01-03 00:11:57 +0900

    FormConfigの移動

--- lib/katana_module.dart
+++ lib/katana_module.dart
@@ -8,8 +8,6 @@
 /// [YouTube]: https://www.youtube.com/c/mathrunetchannel
 library katana_module;
 
-import 'dart:math';
-
 import "package:flutter/material.dart";
 import 'package:katana_routing/katana_routing.dart';
 import 'package:model_notifier/model_notifier.dart';
@@ -37,20 +35,12 @@ part 'src/template_module.dart';
 part 'src/adapter_module.dart';
 part 'src/variable_config.dart';
 part 'src/route_module.dart';
+part 'src/form_config.dart';
 
-part 'src/reroute/reroute_config.dart';
+part 'src/reroute_config.dart';
 part 'src/reroute/login_required_reroute_config.dart';
 part 'src/reroute/registration_required_reroute_config.dart';
 part 'src/reroute/user_document_query_reroute_config.dart';
-
-part 'src/form/form_config.dart';
-part 'src/form/text_form_config.dart';
-part 'src/form/select_form_config.dart';
-part 'src/form/date_time_form_config.dart';
-part 'src/form/multiple_text_form_config.dart';
-part 'src/form/multiple_select_form_config.dart';
-part 'src/form/slider_form_config.dart';
-part 'src/form/hidden_form_config.dart';
 
 part 'src/adapter/ads_adapter.dart';
 part 'src/adapter/dynamic_links_adapter.dart';
